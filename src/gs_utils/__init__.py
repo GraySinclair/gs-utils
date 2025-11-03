a0200d2212576c37a713074061fc59e36532f159
Gray Sinclair <169568836+GraySinclair@users.noreply.github.com>
2025-11-03 13:14:52 -0600

    Replace nb_exit with nb_out in __init__.py

--- src/gs_utils/__init__.py
+++ src/gs_utils/__init__.py
@@ -1,8 +1,8 @@
 from .secrets import get_secret
-from .nb_out import nb_exit
+from .nb_out import nb_out
 from .logging_preset import _configure_logger
 
 
 
-__all__ = ["get_secret", "nb_exit", "_configure_logger"]
+__all__ = ["get_secret", "nb_out", "_configure_logger"]
 # __version__ = "0.2.0"
