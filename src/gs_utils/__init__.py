35a45654a77b68a5f6878bdeb93c7c655604a2b1
Gray Sinclair <169568836+GraySinclair@users.noreply.github.com>
2025-11-03 13:51:49 -0600

    Change import of nb_out from exit_nb module

--- src/gs_utils/__init__.py
+++ src/gs_utils/__init__.py
@@ -1,5 +1,5 @@
 from .secrets import get_secret
-from .nb_out import nb_out
+from .exit_nb import nb_out
 from .get_logger import config_logger
 
 __all__ = ["get_secret", "nb_out", "config_logger"]
