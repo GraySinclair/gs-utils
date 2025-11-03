b64891d2ea0209a77810f8e8202505f341139b68
Gray Sinclair <169568836+GraySinclair@users.noreply.github.com>
2025-11-03 11:41:41 -0600

    Update public API and remove versioning
    Removed 'require_fabric' from public API and versioning.

--- src/gs_utils/__init__.py
+++ src/gs_utils/__init__.py
@@ -1,10 +1,8 @@
-"""Public API for gs_utils."""
-from .runtime import require_fabric
 from .secrets import get_secret
 from .nb_out import nb_exit
 from .logging_preset import _configure_logger
 
 
 
-__all__ = ["require_fabric", "get_secret", "nb_exit", "_configure_logger"]
-__version__ = "0.2.0"
+__all__ = ["get_secret", "nb_exit", "_configure_logger"]
+# __version__ = "0.2.0"
