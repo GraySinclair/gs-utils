be3c9464abd55d739b9320557264a86acc83294b
Gray Sinclair <169568836+GraySinclair@users.noreply.github.com>
2025-11-03 13:51:13 -0600

    Replace logger import and update __all__ list

--- src/gs_utils/__init__.py
+++ src/gs_utils/__init__.py
@@ -1,8 +1,5 @@
 from .secrets import get_secret
 from .nb_out import nb_out
-from .logging_preset import _configure_logger
+from .get_logger import config_logger
 
-
-
-__all__ = ["get_secret", "nb_out", "_configure_logger"]
-# __version__ = "0.2.0"
+__all__ = ["get_secret", "nb_out", "config_logger"]
