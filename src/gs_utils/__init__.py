93a986b73cb1f2dda9afa04d754c3b542067cfe9
Gray Sinclair <169568836+GraySinclair@users.noreply.github.com>
2025-11-05 16:30:45 -0600

    Update __init__.py

--- src/gs_utils/__init__.py
+++ src/gs_utils/__init__.py
@@ -4,6 +4,7 @@ This package provides:
 - get_secret(vault_url, secret_name): fetch AKV secrets via Fabric identity
 - nb_out(status, msg, extras=None): notebook exit with structured JSON payload.
 - config_logger(name, level): opinionated logger preset for notebooks/pipelines
+- is_automated_run(): returns bool-- checks if your notebook is running from a pipeline or manually being run
 """
 from importlib.metadata import version, PackageNotFoundError
 try:
@@ -12,5 +13,6 @@ except PackageNotFoundError:
     __version__ = "0.0.0"
 
 from .secrets import get_secret, nb_out, get_logger
+from .is_automated_run import is_automated_run
 
-__all__ = ["get_secret", "nb_out", "get_logger"]
+__all__ = ["get_secret", "nb_out", "get_logger", "is_automated_run"]
