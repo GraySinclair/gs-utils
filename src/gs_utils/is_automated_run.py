78ebd5b12bb3c3fe259e5167317d69712779cca6
Gray Sinclair <169568836+GraySinclair@users.noreply.github.com>
2025-11-18 09:10:32 -0600

    Update is_automated_run.py

--- src/gs_utils/is_automated_run.py
+++ src/gs_utils/is_automated_run.py
@@ -1,5 +1,5 @@
 
 def is_automated_run() -> bool:
     """Return True if executed from a pipeline activity."""
-    import notebookutils # Type: Ignore
+    import notebookutils  # type: ignore
     return notebookutils.runtime.context.get("isForPipeline")
