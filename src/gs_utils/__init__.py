8898e136d6780a07ceb5290d91adc321d9e99d01
Gray Sinclair <169568836+GraySinclair@users.noreply.github.com>
2025-11-18 19:01:35 -0600

    Update __init__.py

--- src/gs_utils/__init__.py
+++ src/gs_utils/__init__.py
@@ -5,6 +5,7 @@ This package provides:
 - nb_out(status, msg, extras=None): notebook exit with structured JSON payload.
 - config_logger(name, level): opinionated logger preset for notebooks/pipelines
 - is_automated_run(): returns bool-- checks if your notebook is running from a pipeline or manually being run
+- load_cfg(path, maxbytes): loads json; returns dict
 """
 from importlib.metadata import version, PackageNotFoundError
 try:
@@ -13,6 +14,6 @@ except PackageNotFoundError:
     __version__ = "0.0.0"
 
 from .secrets import get_secret, nb_out, get_logger
-from .is_automated_run import is_automated_run
+from .shortcuts import is_automated_run, load_cfg
 
-__all__ = ["get_secret", "nb_out", "get_logger", "is_automated_run"]
+__all__ = ["get_secret", "nb_out", "get_logger", "is_automated_run", "load_cfg"]
